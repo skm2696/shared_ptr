197adc71324aa40cbf0bdb5b1ba2cfb1f01c071d
skm2696 <skm03092014@gmail.com>
2016-11-07 11:02:18 +0400

    Update shared_ptr.h

--- include/shared_ptr.h
+++ include/shared_ptr.h
@@ -7,21 +7,21 @@
 template<typename T>
 class shared_ptr {
 public:
-	shared_ptr() noexcept; 
-	explicit shared_ptr(T * ptr); 
-	shared_ptr(const shared_ptr& r);  
-	shared_ptr(shared_ptr&& r); 
-	auto operator =(const shared_ptr& x)->shared_ptr &;  
-	auto operator =(shared_ptr&& x)->shared_ptr&;  
+	shared_ptr() noexcept; /*noexcept*/
+	explicit shared_ptr(T * ptr); /*strong*/
+	shared_ptr(const shared_ptr& r); /*strong*/
+	shared_ptr(shared_ptr&& r); /*noexcept*/
+	auto operator =(const shared_ptr& x)->shared_ptr &; /*strong*/
+	auto operator =(shared_ptr&& x)->shared_ptr&; /*noexcept*/
 	~shared_ptr(); /*noexcept*/
-	auto reset() noexcept -> void; 
-	auto swap(shared_ptr& s) noexcept -> void; 
-	auto get() const noexcept->T *; 
-	auto use_count() const noexcept->size_t; 
-	auto unique() const noexcept -> bool; 
-	auto operator*() const -> T&;
-	auto operator->() const -> T*;
-	explicit operator bool() const noexcept; 
+	auto reset() noexcept -> void; /*noexcept*/
+	auto swap(shared_ptr& s) noexcept -> void; /*noexcept*/
+	auto get() const noexcept->T *; /*noexcept*/
+	auto use_count() const noexcept->size_t; /*noexcept*/
+	auto unique() const noexcept -> bool; /*noexcept*/
+	auto operator*() const -> T&; /*strong*/
+	auto operator->() const -> T*; /*strong*/
+	explicit operator bool() const noexcept; /*noexcept*/
 private:
 	T * ptr_;
 	size_t * count_;
